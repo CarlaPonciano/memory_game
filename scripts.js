9bb19ef0a6ea068d2a537c5ffaafd5cfb089f66a
CarlaPonciano <si.carla.ponciano@outlook.com>
2024-07-16 22:29:07 -0300

    add congrats screen

--- scripts.js
+++ scripts.js
@@ -5,59 +5,49 @@ document.addEventListener('DOMContentLoaded', () => {
   const modal = document.getElementById('modal');
   const modalImage = document.getElementById('modalImage');
   const modalText = document.getElementById('modalText');
+  const modalTitle = document.getElementById('modalTitle');
   const continueButton = document.getElementById('continueButton');
   const backgroundMusic = document.getElementById('backgroundMusic');
   const audioControlButton = document.getElementById('audioControlButton');
+  const congratsScreen = document.getElementById('congratsScreen');
+  const restartButton = document.getElementById('restartButton');
+  const rotateScreen = document.getElementById('rotateScreen');
+  const confettiContainer = document.getElementById('confettiContainer');
+
   const cardValues = [
-    { value: 'A', image: 'mes_1.jpeg', description: 'Description for A' },
-    { value: 'A', image: 'mes_1.jpeg', description: 'Description for A' },
-    { value: 'B', image: 'mes_2.jpeg', description: 'Description for B' },
-    { value: 'B', image: 'mes_2.jpeg', description: 'Description for B' },
-    { value: 'C', image: 'mes_3.jpeg', description: 'Description for C' },
-    { value: 'C', image: 'mes_3.jpeg', description: 'Description for C' },
-    { value: 'D', image: 'mes_4.jpeg', description: 'Description for D' },
-    { value: 'D', image: 'mes_4.jpeg', description: 'Description for D' },
-    { value: 'E', image: 'mes_5.jpeg', description: 'Description for E' },
-    { value: 'E', image: 'mes_5.jpeg', description: 'Description for E' },
-    { value: 'F', image: 'mes_6.jpeg', description: 'Description for F' },
-    { value: 'F', image: 'mes_6.jpeg', description: 'Description for F' },
-    { value: 'G', image: 'mes_7.jpeg', description: 'Description for G' },
-    { value: 'G', image: 'mes_7.jpeg', description: 'Description for G' },
-    { value: 'H', image: 'mes_8.jpeg', description: 'Description for H' },
-    { value: 'H', image: 'mes_8.jpeg', description: 'Description for H' },
-    { value: 'I', image: 'mes_9.jpeg', description: 'Description for I' },
-    { value: 'I', image: 'mes_9.jpeg', description: 'Description for I' },
-    { value: 'J', image: 'mes_10.jpeg', description: 'Description for J' },
-    { value: 'J', image: 'mes_10.jpeg', description: 'Description for J' },
-    { value: 'K', image: 'mes_11.jpeg', description: 'Description for K' },
-    { value: 'K', image: 'mes_11.jpeg', description: 'Description for K' },
-    { value: 'L', image: 'mes_12.jpeg', description: 'Description for L' },
-    { value: 'L', image: 'mes_12.jpeg', description: 'Description for L' },
+    { value: '1', image: 'mes_1.jpeg', description: 'O dia que você chegou para transformar as nossas vidas Aline! Já podiamos sentir felicidade e um amor inifnito no seu olhar.' },
+    { value: '1', image: 'mes_1.jpeg', description: 'O dia que você chegou para transformar as nossas vidas Aline! Já podiamos sentir felicidade e um amor inifnito no seu olhar.' },
+    { value: '2', image: 'mes_2.jpeg', description: 'Description for B' },
+    { value: '2', image: 'mes_2.jpeg', description: 'Description for B' },
   ];
+  
   let hasFlippedCard = false;
   let firstCard, secondCard;
   let lockBoard = false;
+  let matchedPairs = 0;
 
-    const checkOrientation = () => {
-      if (window.innerHeight > window.innerWidth) {
-        rotateScreen.style.display = 'flex';
-      } else {
-        rotateScreen.style.display = 'none';
-      }
-    };
+  const checkOrientation = () => {
+    if (window.innerHeight > window.innerWidth) {
+      rotateScreen.style.display = 'flex';
+    } else {
+      rotateScreen.style.display = 'none';
+    }
+  };
   
-    window.addEventListener('resize', checkOrientation);
-    checkOrientation();
+  window.addEventListener('resize', checkOrientation);
+  checkOrientation();
 
   startButton.addEventListener('click', startGame);
 
   function startGame() {
+    gameBoard.innerHTML = '';
     document.querySelector('.start-screen').style.display = 'none';
     gameContainer.style.display = 'flex';
     audioControlButton.style.display = 'block';
     createBoard();
     backgroundMusic.volume = 0.5;
     backgroundMusic.play();
+    matchedPairs = 0;
   }
 
   let isPlaying = true;
@@ -109,14 +99,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function checkForMatch() {
     let isMatch = firstCard.dataset.value === secondCard.dataset.value;
-    isMatch ? handleMatch() : unflipCards();
-  }
-
-  function handleMatch() {
-    setTimeout(() => {
-      showModal(firstCard.dataset.value);
-      disableCards();
-    }, 500);
+    if (isMatch) {
+      setTimeout(() => {
+        showModal(firstCard.dataset.value);
+        disableCards();
+      }, 500);
+      matchedPairs++;
+    } else {
+      unflipCards();
+    }
   }
 
   function disableCards() {
@@ -141,18 +132,48 @@ document.addEventListener('DOMContentLoaded', () => {
   function showModal(cardValue) {
     const matchingCard = cardValues.find(card => card.value === cardValue);
     modalImage.src = `./assets/meses/${matchingCard.image}`;
+    modalTitle.textContent = `${matchingCard.value}° mês`
     modalText.textContent = matchingCard.description;
     modal.style.display = 'flex';
   }
 
   continueButton.addEventListener('click', () => {
     modal.style.display = 'none';
+    if (matchedPairs === cardValues.length / 2) {
+      showCongratsScreen();
+    }
   });
 
   function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  function showCongratsScreen() {
+    gameContainer.style.display = 'none';
+    congratsScreen.style.display = 'flex';
+    createConfetti();
+  }
+
+  restartButton.addEventListener('click', () => {
+    congratsScreen.style.display = 'none';
+    gameContainer.style.display = 'flex';
+    startGame();
+  });
+
+  function createConfetti() {
+    const confettiCount = 100;
+    const colors = ['#ff0', '#f00', '#0f0', '#00f', '#ff0', '#f0f', '#0ff'];
+    for (let i = 0; i < confettiCount; i++) {
+      const confetti = document.createElement('div');
+      confetti.classList.add('confetti');
+      confetti.style.left = `${Math.random() * 100}%`;
+      confetti.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+      confettiContainer.appendChild(confetti);
+
+      confetti.style.animationDelay = `${Math.random() * 3}s`;
     }
   }
 });
