6c6c48c879b0155825a1c7648db1aed44c724091
CarlaPonciano <si.carla.ponciano@outlook.com>
2024-07-17 20:17:11 -0300

    add description

--- scripts.js
+++ scripts.js
@@ -15,10 +15,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const confettiContainer = document.getElementById('confettiContainer');
 
   const cardValues = [
-    { value: '1', image: 'mes_1.jpeg', description: 'O dia que você chegou para transformar as nossas vidas Aline! Já podiamos sentir felicidade e um amor inifnito no seu olhar.' },
-    { value: '1', image: 'mes_1.jpeg', description: 'O dia que você chegou para transformar as nossas vidas Aline! Já podiamos sentir felicidade e um amor inifnito no seu olhar.' },
-    { value: '2', image: 'mes_2.jpeg', description: 'Description for B' },
-    { value: '2', image: 'mes_2.jpeg', description: 'Description for B' },
+    { value: '1', image: 'mes_1.jpeg', description: 'A primeira vez que vimos a nossa princesinha! Um momento único que está marcado para sempre, mágico, especial e surpreendente. Aline chegou para transformar as nossas vidas!' },
+    { value: '1', image: 'mes_1.jpeg', description: 'A primeira vez que vimos a nossa princesinha! Um momento único que está marcado para sempre, mágico, especial e surpreendente. Aline chegou para transformar as nossas vidas!' },
+    { value: '2', image: 'mes_2.jpeg', description: 'A cada dia a bonequinha ficava mais linda. Com um lookinho diferente toda hora não parava de nos encantar com tanta beleza e cada sorrisinho era uma explosão de felicidade.' },
+    { value: '2', image: 'mes_2.jpeg', description: 'A cada dia a bonequinha ficava mais linda. Com um lookinho diferente toda hora não parava de nos encantar com tanta beleza e cada sorrisinho era uma explosão de felicidade.' },
+    { value: '3', image: 'mes_3.jpeg', description: 'Sapequinha e bagunceira desde sempre, sempre se divertia com as brincadeiras da família!' },
+    { value: '3', image: 'mes_3.jpeg', description: 'Sapequinha e bagunceira desde sempre, sempre se divertia com as brincadeiras da família!' },
+    { value: '4', image: 'mes_4.jpeg', description: 'Aline até se disfarçava de boneca e todos dizem que é uma bonequinha de tão perfeita em todos os aspectos.' },
+    { value: '4', image: 'mes_4.jpeg', description: 'Aline até se disfarçava de boneca e todos dizem que é uma bonequinha de tão perfeita em todos os aspectos.' },
+    { value: '5', image: 'mes_5.jpeg', description: 'Sempre gosta de posar para as fotos e fazer as melhores caretas que sempre viram figurinhas! Uma verdadeira pop star.' },
+    { value: '5', image: 'mes_5.jpeg', description: 'Sempre gosta de posar para as fotos e fazer as melhores caretas que sempre viram figurinhas! Uma verdadeira pop star.' },
+    { value: '6', image: 'mes_6.jpeg', description: 'Sempre com uma mania diferente, além dos olhos de oceano, ficava surfando e pegando onda! Sempre com um sorriso no olhar.' },
+    { value: '6', image: 'mes_6.jpeg', description: 'Sempre com uma mania diferente, além dos olhos de oceano, ficava surfando e pegando onda! Sempre com um sorriso no olhar.' },
+    { value: '7', image: 'mes_7.jpeg', description: 'Chegou o momento de comer comidinhas, nem sempre foi uma tarefa fácil, mas com certeza foi uma diversão as bagunças.' },
+    { value: '7', image: 'mes_7.jpeg', description: 'Chegou o momento de comer comidinhas, nem sempre foi uma tarefa fácil, mas com certeza foi uma diversão as bagunças.' },
+    { value: '8', image: 'mes_8.jpeg', description: 'Um dos momentos mais especiais quando a princesa recebeu a benção do papai do céu para fortalecer a sua jornada em sua vida!' },
+    { value: '8', image: 'mes_8.jpeg', description: 'Um dos momentos mais especiais quando a princesa recebeu a benção do papai do céu para fortalecer a sua jornada em sua vida!' },
+    { value: '9', image: 'mes_9.jpeg', description: 'A princesinha ama sua mochilinha do cachorro que usa para ir pra escolhinha e fazer mais bagunça ainda! Sempre interessada a descobrir as maravilhas do mundo.' },
+    { value: '9', image: 'mes_9.jpeg', description: 'A princesinha ama sua mochilinha do cachorro que usa para ir pra escolhinha e fazer mais bagunça ainda! Sempre interessada a descobrir as maravilhas do mundo.' },
+    { value: '10', image: 'mes_10.jpeg', description: 'Aline nos diverte em cada momento nos ensinando a aproveitar cada fase com alegria e entrando em suas brincadeiras. Raaaaaarrrrr!!' },
+    { value: '10', image: 'mes_10.jpeg', description: 'Aline nos diverte em cada momento nos ensinando a aproveitar cada fase com alegria e entrando em suas brincadeiras. Raaaaaarrrrr!!' },
+    { value: '11', image: 'mes_11.jpeg', description: 'Fofura sempre está no vocabulário da princesa, e quando achamos que não, Aline fica mais linda e fofa a cada dia que passa.' },
+    { value: '11', image: 'mes_11.jpeg', description: 'Fofura sempre está no vocabulário da princesa, e quando achamos que não, Aline fica mais linda e fofa a cada dia que passa.' },
+    { value: '12', image: 'mes_12.jpeg', description: '1 ano da princesa! Sua jornada até aqui foi incrivelmente espetacular e mal podemos esperar para vivenciar as maravilhas que a vida te reserva, Aline!' },
+    { value: '12', image: 'mes_12.jpeg', description: '1 ano da princesa! Sua jornada até aqui foi incrivelmente espetacular e mal podemos esperar para vivenciar as maravilhas que a vida te reserva, Aline!' },
   ];
   
   let hasFlippedCard = false;
@@ -45,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
     gameContainer.style.display = 'flex';
     audioControlButton.style.display = 'block';
     createBoard();
-    backgroundMusic.volume = 0.5;
+    backgroundMusic.volume = 0.4;
     backgroundMusic.play();
     matchedPairs = 0;
   }
